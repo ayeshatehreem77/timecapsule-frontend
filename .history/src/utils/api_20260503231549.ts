24ec23f3790943e1515f59451b3fff499d998b77
Ayesha Tehreem <ayeshatehreem556@gmail.com>
2026-05-12 16:46:55 +0500

    ...........

--- .history/src/utils/api_20260503231549.ts
+++ .history/src/utils/api_20260503231549.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://time-capsule-16y0.onrender.com",
+  baseURL: "http://localhost:5000",
 });
 
 api.interceptors.request.use((config) => {
